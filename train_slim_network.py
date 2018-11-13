826aab87a528948e6c64cc145beb954ce7d106fa
z <phxv@mail.ru>
2018-11-13 17:51:08 +0300

    1

--- train_slim_network.py
+++ train_slim_network.py
@@ -44,8 +44,8 @@ def model_function(next_element):
 	logits, end_points = inception.inception_v3(
 		x, num_classes=settings.num_classes, is_training=True)
 	loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=y)
-	
-	return loss
+
+	return logits, loss
 
 
 # Create a new graph
@@ -79,9 +79,11 @@ with graph.as_default():
 	acc_top6 = tf.nn.in_top_k(logits, tf.argmax(y,1), 6)
 	"""
 
-	loss = model_function(next_element_train)
+	logits, loss = model_function(next_element_train)
 	train_op = tf.train.AdagradOptimizer(0.01).minimize(loss)
-
+	correct_prediction = tf.equal(tf.argmax(logits,1), tf.argmax(y,1))
+	acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32)) # top-1	
+	acc_top6 = tf.nn.in_top_k(logits, tf.argmax(y,1), 6)
 
 	NUM_EPOCH = 100
 	with tf.Session() as sess:
@@ -94,14 +96,17 @@ with graph.as_default():
 			#	print(labels)
 
 
-			
+			i = 0
 			while True:
+				i += 1
 				try:
 					#batch = sess.run(next_batch_train)
 					#features = batch[0]
 					#labels = batch[1]
 					#print(labels)
 					sess.run(train_op)
+					train_acc = acc.eval(feed_dict={x: batch[0], y: batch[1]})
+					print('epoch={0} i={1} train_acc={2:.4f}'.format(epoch, i, train_acc))
 
 				except tf.errors.OutOfRangeError:
 					print("End of training dataset.")
