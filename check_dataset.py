2bc46938b925daee93e0232b5c960fd82b833868
z <phxv@mail.ru>
2018-11-18 15:01:22 +0300

    1

---
+++ check_dataset.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Прямой проход без хеширования промежуточных данных.
+
+v2 - added saver.
+"""
+	
+# https://github.com/tensorflow/tensorflow/issues/22837#issuecomment-428327601
+
+import tensorflow as tf
+import numpy as np
+import math
+import sys, os
+
+from dataset_factory import GoodsDataset
+#from goods_tf_records import GoodsTfrecordsDataset
+
+# tf.enable_eager_execution()
+import settings
+from settings import IMAGE_SIZE
+from utils.timer import timer
+
+#--
+#------------
+# dataset
+goods_dataset = GoodsDataset(settings.dataset_list, settings.labels_list, 
+settings.IMAGE_SIZE, settings.train_batch, settings.valid_batch, settings.multiply, 
+settings.valid_percentage)
+
+train_dataset = goods_dataset.get_train_dataset()
+valid_dataset = goods_dataset.get_valid_dataset()
+
+graph = tf.Graph()  # сreate a new graph
+
+with graph.as_default():
+	
+	iterator_train = train_dataset.make_one_shot_iterator()
+	next_element_train = iterator_train.get_next()
+	iterator_valid = valid_dataset.make_one_shot_iterator()
+	next_element_valid = iterator_valid.get_next()
+
+	count = 0
+	while True:				
+		try:
+			features, labels = sess.run(next_element_train)
+			count += 1					
+		except tf.errors.OutOfRangeError:
+			print("End of training dataset. Count={}".format(count))
+			break	
