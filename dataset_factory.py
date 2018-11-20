506dd84184f091657ff4cf2e8a700100a060a32a
z <phxv@mail.ru>
2018-11-20 16:53:54 +0300

    1

--- dataset_factory.py
+++ dataset_factory.py
@@ -269,7 +269,7 @@ class GoodsDataset:
                 images = tf.contrib.image.transform(images, transform1)            
                 images = tf.image.resize_image_with_crop_or_pad(images, h, w)
                 # ---            
-                zoom = settings.crop_zoom
+                zoom = 1.2
                 w_crop = math.ceil(w / zoom)
                 h_crop = math.ceil(h / zoom)
                 #batch_size = int(images.shape[0])
