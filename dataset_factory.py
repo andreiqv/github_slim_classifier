7dd55f076d437603b8bd54842a2953ea5d60d081
z <phxv@mail.ru>
2018-11-20 23:28:53 +0300

    1

--- dataset_factory.py
+++ dataset_factory.py
@@ -233,54 +233,55 @@ class GoodsDataset:
             # angle = tf.random_uniform(shape=(1,), minval=0, maxval=90)
             # images = tf.contrib.image.rotate(images, angle * math.pi / 180, interpolation='BILINEAR')
 
-            if settings.do_transformations:
-                # Rotation and transformation
-                # print(images.shape)  # = (?, 299, 299, ?)
-                print('images.shape:', images.shape)      
-                w, h = IMAGE_SIZE
-                a = max(w, h)
-                d = math.ceil(a * (math.sqrt(2) - 1) / 2)
-                print('paddings d =', d)
-                paddings = tf.constant([[0, 0], [d, d], [d, d], [0, 0]])
-                images = tf.pad(images, paddings, "SYMMETRIC")
-                #images = tf.image.resize_image_with_crop_or_pad(images, w+d, h+d)
-                print('images.shape:', images.shape)
-                angle = tf.random_uniform(shape=(1,), minval=0, maxval=settings.rotation_max_angle)
-                
-                images = tf.contrib.image.rotate(images, angle * math.pi / 180, interpolation='BILINEAR')
-                
-                #images = tf.image.crop_to_bounding_box(images, d, d, s+d, s+d)
-                                
-                # Transformation
-                #transform1 = tf.constant([1.0, 0.2, -30.0, 0.2, 1.0, 0.0, 0.0, 0.0], dtype=tf.float32)            
-                # transform is  vector of length 8 or tensor of size N x 8
-                # [a0, a1, a2, b0, b1, b2, c0, c1]            
-                a0 = tf.constant([1.0])
-                a1 = tf.random_uniform(shape=(1,), minval=0.0, maxval=settings.transform_maxval)
-                a2 = tf.constant([-30.0])
-                b0 = tf.random_uniform(shape=(1,), minval=0.0, maxval=settings.transform_maxval)
-                b1 = tf.constant([1.0])
-                b2 = tf.constant([-30.0])
-                c0 = tf.constant([0.0])
-                c1 = tf.constant([0.0])
-                transform1 = tf.concat(axis=0, values=[a0, a1, a2, b0, b1, b2, c0, c1])
-                #transform = tf.tile(tf.expand_dims(transform1, 0), [batch, 1])
-                #print('Added transformations:', transform)
-                images = tf.contrib.image.transform(images, transform1)            
-                images = tf.image.resize_image_with_crop_or_pad(images, h, w)
-                # ---            
-                zoom = 1.1
-                w_crop = math.ceil(w / zoom)
-                h_crop = math.ceil(h / zoom)
-                #batch_size = int(images.shape[0])
-                #print(images.shape)
-                batch_size = tf.size(images) / (3*h*w)
-                images = tf.random_crop(images, [batch_size, h_crop, w_crop, 3])
+            # Rotation and transformation
+            # print(images.shape)  # = (?, 299, 299, ?)
+            print('images.shape:', images.shape)      
+            w, h = IMAGE_SIZE
+            a = max(w, h)
+            d = math.ceil(a * (math.sqrt(2) - 1) / 2)
+            print('paddings d =', d)
+            paddings = tf.constant([[0, 0], [d, d], [d, d], [0, 0]])
+            images = tf.pad(images, paddings, "SYMMETRIC")
+            #images = tf.image.resize_image_with_crop_or_pad(images, w+d, h+d)
+            print('images.shape:', images.shape)
+            angle = tf.random_uniform(shape=(1,), minval=0, maxval=settings.rotation_max_angle)
                 
 
-                images = tf.image.resize_images(images, [h, w])            
-                # ---
-                # end of Rotation and Transformation block   
+            #    
+            images = tf.contrib.image.rotate(images, angle * math.pi / 180, interpolation='NEAREST')
+            #images = tf.contrib.image.rotate(images, angle * math.pi / 180, interpolation='BILINEAR')
+            
+            #images = tf.image.crop_to_bounding_box(images, d, d, s+d, s+d)
+                                
+            # Transformation
+            #transform1 = tf.constant([1.0, 0.2, -30.0, 0.2, 1.0, 0.0, 0.0, 0.0], dtype=tf.float32)            
+            # transform is  vector of length 8 or tensor of size N x 8
+            # [a0, a1, a2, b0, b1, b2, c0, c1]            
+            a0 = tf.constant([1.0])
+            a1 = tf.random_uniform(shape=(1,), minval=0.0, maxval=settings.transform_maxval)
+            a2 = tf.constant([-30.0])
+            b0 = tf.random_uniform(shape=(1,), minval=0.0, maxval=settings.transform_maxval)
+            b1 = tf.constant([1.0])
+            b2 = tf.constant([-30.0])
+            c0 = tf.constant([0.0])
+            c1 = tf.constant([0.0])
+            transform1 = tf.concat(axis=0, values=[a0, a1, a2, b0, b1, b2, c0, c1])
+            #transform = tf.tile(tf.expand_dims(transform1, 0), [batch, 1])
+            #print('Added transformations:', transform)
+            images = tf.contrib.image.transform(images, transform1)            
+            images = tf.image.resize_image_with_crop_or_pad(images, h, w)
+            # ---            
+            zoom = 1.1
+            w_crop = math.ceil(w / zoom)
+            h_crop = math.ceil(h / zoom)
+            #batch_size = int(images.shape[0])
+            #print(images.shape)
+            batch_size = tf.size(images) / (3*h*w)
+            images = tf.random_crop(images, [batch_size, h_crop, w_crop, 3])
+
+            images = tf.image.resize_images(images, [h, w])            
+            # ---
+            # end of Rotation and Transformation block   
 
             
             """
@@ -300,8 +301,8 @@ class GoodsDataset:
             noise = tf.random_normal(shape=tf.shape(images), mean=0.0, stddev=0.1, dtype=tf.float32)
             images = tf.add(images, noise)
 
-            images = tf.image.per_image_standardization(images)
-            images = tf.map_fn(lambda frame: tf.image.per_image_standardization(frame), images) 
+            #images = tf.image.per_image_standardization(images)
+            #images = tf.map_fn(lambda frame: tf.image.per_image_standardization(frame), images) 
             
             images = tf.minimum(images, 1.0)
             images = tf.maximum(images, 0.0)
