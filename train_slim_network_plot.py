d9a7b0f1f227b4647874207f404f2a1d40236e00
z <phxv@mail.ru>
2018-11-21 12:16:41 +0300

    1

--- train_slim_network_plot.py
+++ train_slim_network_plot.py
@@ -37,9 +37,10 @@ slim = tf.contrib.slim
 from nets import simple_fc
 #net, net_model_name = simple_fc.fc, 'simple_fc'
 #net, net_model_name = alexnet.alexnet_v2, 'alexnet_v2'
+#net, net_model_name = inception_v4.inception_v4, 'inception_v4'
 #net, net_model_name = resnet_v2.resnet_v2_50, 'resnet_v2_50'
+net, net_model_name = resnet_v2.resnet_v2_152, 'resnet_v2_152'
 
-net, net_model_name = inception_v4.inception_v4, 'inception_v4'
 
 #net = inception.inception_v3
 #net = inception.inception_v4
