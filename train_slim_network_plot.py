5b5e185f2594633d39f98857e9e404dc44beaebc
z <phxv@mail.ru>
2018-11-21 22:13:19 +0300

    1

--- train_slim_network_plot.py
+++ train_slim_network_plot.py
@@ -39,7 +39,8 @@ from nets import simple_fc
 #net, net_model_name = alexnet.alexnet_v2, 'alexnet_v2'
 #net, net_model_name = inception_v4.inception_v4, 'inception_v4'
 #net, net_model_name = resnet_v2.resnet_v2_50, 'resnet_v2_50'
-net, net_model_name = resnet_v2.resnet_v2_152, 'resnet_v2_152'
+#net, net_model_name = resnet_v2.resnet_v2_152, 'resnet_v2_152'
+net, net_model_name = mobilenet_v2.mobilenet_v2, 'mobilenet_v2'
 
 
 #net = inception.inception_v3
