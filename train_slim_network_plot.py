7b3f43030ca0612e9ba881917b795656979343bb
z <phxv@mail.ru>
2018-11-22 17:50:30 +0300

    1

--- train_slim_network_plot.py
+++ train_slim_network_plot.py
@@ -164,10 +164,10 @@ if __name__ == '__main__':
 
 		#x = images_augment(x)
 
-		with tf.device("/device:GPU:1"):	
-			logits, end_points = net(x, num_classes=num_classes, is_training=True)
-			logits = tf.reshape(logits, [-1, num_classes])
-			output = tf.nn.softmax(logits, name=OUTPUT_NODE)
+		#with tf.device("/device:GPU:0"):	
+		logits, end_points = net(x, num_classes=num_classes, is_training=True)
+		logits = tf.reshape(logits, [-1, num_classes])
+		output = tf.nn.softmax(logits, name=OUTPUT_NODE)
 
 		loss = tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=y)
 		train_op = tf.train.AdagradOptimizer(0.01).minimize(loss)
